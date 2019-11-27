9716bdbb743af580eb7849343ac17c7c4891ae9f
Faye-Lino Agli <lino.agli@gmail.com>
2019-11-27 18:19:16 -0500

    - This is a now a multi-project repo: gamepad-intercepts + gamepad-intercepts-bootstrapper

---
+++ gamepad-intercepts-bootstrapper/GamePad Intercepts Bootstrapper/App.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GamePad_Intercepts_Bootstrapper
+{
+    class App : ApplicationContext
+    {
+        public static readonly string PATH_FILE_GAMEPAD_INTERCEPTS = @"C:\GamePad Intercepts\bin\GamePad Intercepts.exe";
+        public static readonly string PATH_FILE_XADDITUS = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"xadditus_app\xadditus_app.exe");
+
+        public App()
+        {
+            // TODO: implement a heartbeat monitor for the processes
+
+            // Launching GamePad Intercepts as an admin
+            ProcessStartInfo gamePadInterceptProcessInfo = new ProcessStartInfo()
+            {
+                FileName = PATH_FILE_GAMEPAD_INTERCEPTS,
+                UseShellExecute = true,
+                Arguments = "",
+                Verb = "runas"
+            };
+            Process.Start(gamePadInterceptProcessInfo);
+
+            // Launching Xadditus as an admin
+            ProcessStartInfo xadditusProcessInfo = new ProcessStartInfo()
+            {
+                FileName = PATH_FILE_XADDITUS,
+                UseShellExecute = true,
+                Arguments = "",
+                Verb = "runas"
+            };
+            Process.Start(xadditusProcessInfo);
+        }
+    }
+}
---
+++ gamepad-intercepts-bootstrapper/GamePad Intercepts Bootstrapper/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GamePad_Intercepts_Bootstrapper
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new App());
+        }
+
+        private static void UIThreadException(object sender, ThreadExceptionEventArgs t)
+        {
+            DialogResult result = DialogResult.Cancel;
+            try
+            {
+                string errorMsg = "An application error occurred. Please contact the adminstrator with the following information:\n\n";
+                errorMsg = errorMsg + t.Exception.Message + "\n\nStack Trace:\n" + t.Exception.StackTrace;
+                result = MessageBox.Show(errorMsg, "Windows Forms Error", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Stop);
+            }
+            catch
+            {
+                try
+                {
+                    MessageBox.Show("Fatal Windows Forms Error",
+                        "Fatal Windows Forms Error", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Stop);
+                }
+                finally
+                {
+                    Application.Exit();
+                }
+            }
+
+            // Exits the program when the user clicks Abort.
+            if (result == DialogResult.Abort) Application.Exit();
+        }
+
+        private static void UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                Exception ex = (Exception)e.ExceptionObject;
+                string errorMsg = "An application error occurred. Please contact the adminstrator with the following information:\n\n";
+                MessageBox.Show(errorMsg + ex.Message + "\n\nStack Trace:\n" + ex.StackTrace, "Unexcpected Non-UI Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            catch (Exception exc)
+            {
+                try
+                {
+                    MessageBox.Show("Fatal Non-UI Error. Could not write the error to the event log. Reason: " + exc.Message, "Fatal Non-UI Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                }
+                finally
+                {
+                    Application.Exit();
+                }
+            }
+            finally
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
